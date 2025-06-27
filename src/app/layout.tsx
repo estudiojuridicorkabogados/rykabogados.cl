48c97857f1c65ac801435c62bfa3e7aa1d6ba0de
Davide Crestini <davide.crestini94@gmail.com>
2025-06-27 12:52:11 -0600

    chore: added zapier chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +13,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const ZAPIER_CHATBOT_ID = "cmccfrrp7003wj4mb8rbq0jed";
 
 export const metadata: Metadata = {
   title: "RyK Abogados | Excelencia, Lealtad y Integridad",
@@ -25,7 +28,8 @@ export const metadata: Metadata = {
     "abogados chile santiago",
     "abogados r&k",
   ],
-    description: "Estudio jurídico especializado en derecho laboral con servicios en todo Chile. Ofrecemos excelencia, lealtad e integridad en cada caso.",
+  description:
+    "Estudio jurídico especializado en derecho laboral con servicios en todo Chile. Ofrecemos excelencia, lealtad e integridad en cada caso.",
 };
 
 export default function RootLayout({
@@ -34,13 +38,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
 
         <Footer />
+
+        <Script
+          async
+          type="module"
+          src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<zapier-interfaces-chatbot-embed is-popup="true" chatbot-id="${ZAPIER_CHATBOT_ID}"></zapier-interfaces-chatbot-embed>`
+          }}
+        />
       </body>
     </html>
   );
