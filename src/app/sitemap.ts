e79bacbe6ef1f1e5604e5bd5b45e6a84ff2d84c2
Davide Crestini <davide.crestini94@gmail.com>
2025-08-08 11:45:26 +0200

    chore: updatd node version

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-import { ApiPost } from "@/types/global";
+// import { ApiPost } from "@/types/global";
 
 const BASE_PATHS = [
   "",
@@ -11,16 +11,17 @@ const BASE_PATHS = [
 ];
 
 async function fetchPostRoutes() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/post`, {
-    next: { revalidate: 30 },
-  });
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/post`, {
+  //   next: { revalidate: 30 },
+  // });
 
-  const { posts } = await res.json();
+  // const { posts } = await res.json();
 
-  return posts.map((post: { slug: string; lastModified: Date }) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.slug}`,
-    lastModified: post.lastModified,
-  }));
+  // return posts.map((post: { slug: string; lastModified: Date }) => ({
+  //   url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.slug}`,
+  //   lastModified: post.lastModified,
+  // }));
+  return []
 }
 
 export default async function sitemap() {
