e90d261322ea56da72f136cf4796e2ef8a63a1de
Davide Crestini <davide.crestini94@gmail.com>
2025-09-11 19:21:12 +0200

    fix: popover

--- src/components/Navbar/HoverPopover.tsx
+++ src/components/Navbar/HoverPopover.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { Popover } from "radix-ui";
 import { ChevronDown } from "lucide-react";
 import { Route } from "next";
 import Link from "next/link";
 
 import { classNames } from "@/lib/utils/classNames";
+import { useState } from "react";
 
 interface HoverPopoverProps {
   label: string;
@@ -18,42 +19,44 @@ interface HoverPopoverProps {
 }
 
 export const HoverPopover: React.FC<HoverPopoverProps> = ({ label, links }) => {
-  return (
-    <Popover className="relative">
-      {({ open }) => (
-        <>
-          <PopoverButton className="text-black hover:text-accent-dark transition-colors cursor-pointer focus:outline-0 active:outline-0 flex items-center gap-2">
-            {label}
-            <ChevronDown
-              className={classNames("size-4 transition-rotate duration-500", {
-                "rotate-180": open,
-              })}
-            />
-          </PopoverButton>
+  const [open, setOpen] = useState(false);
 
-          <PopoverPanel
-            transition
-            anchor="bottom start"
-            className="mt-2 flex flex-col justify-center bg-white z-50 top-5 py-2 px-4 rounded-sm border-black/10 border-1 data-closed:-translate-y-1 data-closed:opacity-0 transition duration-200 ease-in-out"
-          >
-            {links.map(({ href, label, description, icon: Icon }) => (
-              <Link
-                key={label}
-                href={href}
-                className="text-black hover:text-accent-dark transition-all flex items-center gap-3 first:border-b first:border-black/10 py-3 first:pb-5 last:pt-4"
-              >
-                <div className="flex items-center justify-center rounded-sm">
-                  {Icon}
-                </div>
-                <div className="flex flex-col gap-x justify-center">
-                  <span className="text-sm">{label}</span>
-                  {/* <span className="text-xs">{description}</span> */}
-                </div>
-              </Link>
-            ))}
-          </PopoverPanel>
-        </>
-      )}
-    </Popover>
+  const handleLinkClick = () => {
+    setTimeout(() => {
+      setOpen(false);
+    }, 200);
+  };
+
+  return (
+    <Popover.Root open={open} onOpenChange={setOpen}>
+      <Popover.Trigger className="text-black hover:text-accent-dark transition-colors cursor-pointer focus:outline-0 active:outline-0 flex items-center gap-2">
+        {label}
+        <ChevronDown
+          className={classNames("size-4 transition-rotate duration-500", {
+            "rotate-180": open,
+          })}
+        />
+      </Popover.Trigger>
+
+      <Popover.Portal>
+        <Popover.Content className="mt-2 flex flex-col justify-center bg-white z-50 py-2 px-4 rounded-sm border-black/10 border-1 transition duration-200 ease-in-out">
+          {links.map(({ href, label, icon: Icon }) => (
+            <Link
+              key={label}
+              href={href}
+              className="text-black hover:text-accent-dark transition-all flex items-center gap-3 first:border-b first:border-black/10 py-3 first:pb-5 last:pt-4"
+              onClick={handleLinkClick}
+            >
+              <div className="flex items-center justify-center rounded-sm">
+                {Icon}
+              </div>
+              <div className="flex flex-col gap-x justify-center">
+                <span className="text-sm">{label}</span>
+              </div>
+            </Link>
+          ))}
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 };
