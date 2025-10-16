3be9df1742c8c8831cb13fd8502cf69d6809eedc
Davide Crestini <davide.crestini94@gmail.com>
2025-10-16 11:52:16 +0300

    chore: updated robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/api/",
     },
-    sitemap: "https://rykabogados.cl/sitemap.xml",
+    sitemap: "https://rkabogados.cl/sitemap.xml",
   };
 }
