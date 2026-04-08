15af28eac258e67a01b4a55cdbee1d0819ee05bf
RK Abogados <estudio.juridico@rkabogados.cl>
2026-04-07 23:15:57 -0400

    chore: updated copy on videos  for empresas

--- src/app/habla-con-nosotros/empresas/_components/Videos/constants.ts
+++ src/app/habla-con-nosotros/empresas/_components/Videos/constants.ts
@@ -8,23 +8,24 @@ export interface Video {
 export const VIDEOS: Video[] = [
   {
     id: "1",
-    title: "¿Qué es la subcontratación?",
+    title:
+      "En RK Abogados asesoramos a empresas en procesos de desvinculación laboral:",
     description:
-      " Es cuando trabajas en una empresa, pero a beneficio de otra. En estos casos, ambas son responsables de tus derechos laborales. Dudas y discusiones. ¿Es subcontratación? ¡Conversemos!",
+      "Redactamos cartas de despido, calculamos indemnizaciones y brindamos apoyo ante contingencias conforme a la normativa vigente.",
     videoUrl: "https://www.youtube.com/shorts/4L6DvTapUA0",
   },
   {
     id: "2",
-    title: "¿Qué significa tener fuero sindical?",
+    title: "En RK Abogados, prestamos asesoría preventiva en materia laboral:",
     description:
-      "Significa protección frente al despido. Si eres dirigente sindical, no te pueden desvincular sin la autorización de un tribunal laboral, aún por causas justificadas. Esto es un derecho clave para que puedas ejercer la representación sin miedo ni presión. Si necesitas ayuda sobre esta materia, no dudes en contactarte con nosotros.",
+      "Revisamos contratos, reglamentos internos y nos encargamos del cumplimiento normativo de tu empresa.",
     videoUrl: "https://youtube.com/shorts/UBB7fy4mRn4?si=j1TxLm_-8ItmbOeO",
   },
   {
     id: "3",
-    title: "¿Te han acusado de falta de probidad en el trabajo? ",
+    title: "En RK Abogados defendemos a empresas frente a demandas laborales:",
     description:
-      "Cuidado: es una de las causales más graves de despido y permite al empleador no pagar indemnización alguna. Pero, ¿es siempre justa? La falta de probidad es una acusación seria, pero también es mal utilizada por algunos empleadores para desvincular a trabajadores sin pagar lo que les corresponde.",
+      "Evaluamos la estrategia, riesgos y alternativas, acompañándote durante todo el proceso judicial.",
     videoUrl: "https://youtube.com/shorts/zAurtGjy-oc?si=29mHqtlIvBXd2exm",
   },
 ];
