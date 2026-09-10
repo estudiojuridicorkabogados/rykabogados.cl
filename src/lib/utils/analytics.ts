e2aa25499272f8dd26e4cd5e1f7eea6c5370b0e1
RK Abogados <estudio.juridico@rkabogados.cl>
2026-09-10 23:20:44 +0300

    chore: updated form events

--- src/lib/utils/analytics.ts
+++ src/lib/utils/analytics.ts
@@ -1,14 +1,16 @@
 const BASE_CONVERSION_ID = "AW-11083927345";
 
-const CONTACT_FORM_CONVERSION_ID = "yTHYCP_x0MobELGenaUp";
-const WHATSAPP_CONVERSION_ID = "yTHYCP_x0MobELGenaUp";
-const EMPRESAS_BOOK_A_CALL_CONVERSION_ID = "yTHYCP_x0MobELGenaUp";
-const TRABAJADORES_BOOK_A_CALL_CONVERSION_ID = "yTHYCP_x0MobELGenaUp";
+const CONTACT_FORM_CONVERSION_ID = "_AbcCMzlkfIcELGenaUp";
+const WHATSAPP_CONVERSION_ID = "GhpoCLOFkvIcELGenaUp";
+const EMPRESAS_BOOK_A_CALL_CONVERSION_ID = "tfBhCLX2nPMcELGenaUp";
+const TRABAJADORES_BOOK_A_CALL_CONVERSION_ID = "KGx4CMKMpfMcELGenaUp";
 
 function trackConversion(sendToId: string) {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
     window.gtag("event", "conversion", {
       send_to: `${BASE_CONVERSION_ID}/${sendToId}`,
+      value: 1.0,
+      currency: "CLP",
     });
   }
 }
